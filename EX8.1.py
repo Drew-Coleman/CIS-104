221336c7df7c38f669733f76d96ce4827d6a9b19
Drew-Coleman <113147994+Drew-Coleman@users.noreply.github.com>
2023-04-17 17:45:00 -0400

    Add files via upload

---
+++ EX8.1.py
@@ -0,0 +1,15 @@
+def chop(lst):
+    del lst[0]                       
+    del lst[-1]                       
+def middle(lst):
+    new = lst[1:]                 
+    del new[-1]                     
+    return new
+my_list = [1, 2, 3, 4]
+my_list2 = [1, 2, 3, 4]
+chop_list = chop(my_list)
+print(my_list)                  
+print(chop_list)                      
+middle_list = middle(my_list2)
+print(my_list2)                        
+print(middle_list)                      
